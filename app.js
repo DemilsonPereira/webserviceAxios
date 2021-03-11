837672afcb3575d8403823079121b0400bd2a4d7
Demilson Pereira <demilson.d2102@gmail.com>
2021-03-10 21:59:50 -0400

    Feito a desestruturação para pegar campos específicos.

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ import axios from "axios";
 
 class Api {
   static async getEndereco(cep) {
-    const res = await (axios.get(`https://viacep.com.br/ws/${cep}/json/`));
-    console.log('response', res)
+    // const res = (await axios.get(`https://viacep.com.br/ws/${cep}/json/`)).data;
+    // console.log('response', res)
+
+    const res = new Endereco(
+      (await axios.get(`https://viacep.com.br/ws/${cep}/json/`)).data
+    );
+    return res;
   }
 }
 
-Api.getEndereco(69035430);
+// REALIZANDO A DESESTRUTURAÇÃO DOS DADOS,PARA PEGANDO OS CAMPOS ESPECÍFICOS
+class Endereco {
+  constructor({ logradouro, bairro, localidade }) {
+    (this.logradouro = logradouro),
+      (this.bairro = bairro),
+      (this.localidade = localidade);
+  }
+}
+
+Api.getEndereco(69035430).then((valor) => {
+  console.log(valor);
+});
